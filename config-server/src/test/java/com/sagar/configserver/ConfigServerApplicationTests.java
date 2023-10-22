53dec997088c6eac466f0d958c8366d11cb6418f
Sagar Gavhane <sgavhane70@gmail.com>
2023-10-23 00:12:52 +0530

    feat: add config-service

---
+++ config-server/src/test/java/com/sagar/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
